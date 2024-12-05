20936f8fb16c4b68464d924aff9a5ae8a8bb30b4
Árnilsen Arthur Castilho Lopes <arnilsenarthur@gmail.com>
2024-12-05 20:08:56 -0300

    Showcase

--- Assets/Video/ShowcaseController.cs
+++ Assets/Video/ShowcaseController.cs
@@ -1,5 +1,7 @@
 using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Video
 {
@@ -8,32 +10,57 @@ namespace Video
         [Serializable]
         public struct ShowcaseData
         {
-            public GameObject o;
+            public GameObject[] o;
             public string name;
             public string description;
             public bool spin;
             public Vector3 spinAxis;
+            public float time;
         }
         
         public float spinSpeed = 1f;
         public ShowcaseData[] showcaseData;
         public int currentShowcaseIndex = 0;
         private int _lastShowcaseIndex = -1;
-        
+        public TMP_Text text;
+        public Material material;
+        public float timeOffset = 10f;
+        private static readonly int _TimeOffset = Shader.PropertyToID("_TimeOffset");
+
+        private float _timer = 0f;
+
         private void Update()
         {
+            _timer += Time.deltaTime;
+            if (_timer > showcaseData[currentShowcaseIndex].time)
+            {
+                _timer = 0f;
+                currentShowcaseIndex = (currentShowcaseIndex + 1) % showcaseData.Length;
+            }
+            
+            if(Keyboard.current.aKey.wasPressedThisFrame)
+                currentShowcaseIndex = (currentShowcaseIndex + 1) % showcaseData.Length;
+            
             if (currentShowcaseIndex != _lastShowcaseIndex)
             {
                 _lastShowcaseIndex = currentShowcaseIndex;
                 for (var i = 0; i < showcaseData.Length; i++)
                 {
-                    showcaseData[i].o.SetActive(i == currentShowcaseIndex);
+                    foreach(var go in showcaseData[i].o)
+                        go.SetActive(i == currentShowcaseIndex);
                 }
+                
+                var title = showcaseData[currentShowcaseIndex].name;
+                var desc = showcaseData[currentShowcaseIndex].description;
+                text.text = $"<size=70><b>{title}</b></size>\n{desc}";
+                
+                material.SetFloat("_TimeOffset", material.GetFloat(_TimeOffset) + timeOffset);
             }
             
             if (showcaseData[currentShowcaseIndex].spin)
             {
-                showcaseData[currentShowcaseIndex].o.transform.localEulerAngles = showcaseData[currentShowcaseIndex].spinAxis * (Time.time * spinSpeed);
+                foreach(var go in showcaseData[currentShowcaseIndex].o)
+                    go.transform.localEulerAngles = showcaseData[currentShowcaseIndex].spinAxis * (Time.time * spinSpeed);
             }
         }
         
